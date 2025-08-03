257bade3197097091baaac7faa60f0e8fe01440c
del3500 <wendellsmesia@gmail.com>
2025-08-04 04:24:25 +0800

    added Metadata

--- us811/street.py
+++ us811/street.py
@@ -1,15 +1,40 @@
 from helpers import Pole, parse_csv
+from typing import TypedDict
 import requests
-from requests import Response
 
 
-# Get the streetname of a given GPS coordinate
-def reverse_geocode(gps: Pole) -> Response:
+class Metadata(TypedDict):
+    street_name: str
+    osm_id: int
+    county: str
+    city: str
+
+
+"""
+This will return a dictionary with the needed data for other usages 
+Needed data:
+street name
+osm_id (to get the intersecition using the OVERPASS API)
+county
+name: city
+"""
+
+
+def reverse_geocode(gps: Pole) -> Metadata:
     base_url = "https://nominatim.openstreetmap.org/reverse"
     params = {"lat": gps["lat"], "lon": gps["lon"], "format": "json"}
-    headers = {'User-Agent': "us811/v1"}
+    headers = {"User-Agent": "us811/v1"}
+
     response = requests.get(base_url, params=params, headers=headers)
-    return response.text
+    json = response.json()
+    print(json)
+
+    pole_metadata: Metadata = {
+        "osm_id": int(json["osm_id"]),
+        "county": json["address"]["county"],
+        "city": json["address"]["road"],
+    }
+    return pole_metadata
 
 
 list_of_poles = parse_csv("../tests/test_data.csv")
