bb71d6179bc2352e0a7e76ccd9a07c2fcc48142c
del3500 <wendellsmesia@gmail.com>
2025-08-17 00:06:49 +0800

    added toml, bug fixes, updated solution

--- us811/helpers.py
+++ us811/helpers.py
@@ -1,16 +1,21 @@
-from typing import TypedDict
-from dotenv import load_dotenv
-from pathlib import Path
 import csv
 import json
 import os
+from pathlib import Path
+from typing import Any, TypedDict
+
 import requests
+from dotenv import load_dotenv
 
 
 class Pole(TypedDict):
     structnum: str
     lat: float
     lon: float
+    street: str
+    intersection: str
+    county: str
+    verbiage: str
 
 
 env_path = Path(__file__).resolve().parent.parent / ".env"
@@ -20,7 +25,7 @@ api_key = os.getenv("API_KEY")
 
 def parse_csv(file_path: str) -> list[Pole]:
     poles: list[Pole] = []
-    with open(file_path, mode="r") as csv_file:
+    with open(file_path) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             pole_no: Pole = {
@@ -32,53 +37,36 @@ def parse_csv(file_path: str) -> list[Pole]:
     return poles
 
 
-# https://api.mapbox.com/search/searchbox/v1/reverse?longitude={longitude}&latitude={latitude}
-"""
-def searchbox():
-    base_url = "https://api.mapbox.com/search/searchbox/v1/reverse?longitude=-92.316870&latitude=34.727450"
-    params: dict[str, str] = {"types": "street", "access_token": api_key}
+# Full solution:
+# query tileset using the gps coordinate
+# get the 2 first indices of feature[] list IF AND ONLY IF key "name" exists inside property dict/json.
+# get the "name" key inside dict "property" for street names
+# get the gps coordinates of the 2 indices
+#    - geometry{[coordinates]}
+# query the directions api using the 2 indices (lonlat origin; lotlat dest)
+# get the first index of "routes",  -> "legs" -> "steps" ->
+# check the "steps"{"name": "str"}
+# if "steps" name != street name, get value of "name" and "distance"
 
-    response = requests.get(base_url, params=params)
-    print(json.dumps(response.json()))
-"""
 
 # https://api.mapbox.com/v4/{tileset_id}/tilequery/{lon},{lat}.json
-"""
-def tilequery():
-    base_url: str = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/-92.316870,34.727450.json"
-    params: dict[str, str | int] = {
+def tilequery(poles: list[Poles]) -> list[Poles]:
+    base_url: str = "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/-92.311262,34.738984.json"
+    params: dict[str, str | Any] = {
         "radius": 500,
         "limit": 10,
+        "geometry": "linestring",
+        "dedupe": False,
         "types": "street",
         "layers": "road",
-        "access_token": api_key
+        "access_token": api_key,
     }
-
     response = requests.get(base_url, params=params)
-    print(json.dumps(response.json()))
-"""
-
-
-# https://api.mapbox.com/search/geocode/v6/reverse?longitude={longitude}&latitude={latitude}
-"""
-def geocodev6():
-    base_url: str = "https://api.mapbox.com/search/geocode/v6/reverse?longitude=-92.316870&latitude=34.727450"
-    params: dict[str, str] = {"types": "street", "access_token": api_key}
-    response = requests.get(base_url, params=params)
-    print(json.dumps(response.json()))
-"""
-
-
-# https://api.mapbox.com/matching/v5/{profile}/{coordinates}.json
-def mapmatch():
-    base_url: str = (
-        "https://api.mapbox.com/matching/v5/mapbox/driving/-92.316870,34.727450;-92.316870,34.727450.json"
-    )
-    params: dict[str, str] = {"access_token": api_key}
-    response = requests.get(base_url, params=params)
-    print(response.status_code)
-    print(json.dumps(response.json()))
+    data = response.json()
+    print(json.dumps(data))
+    return data
 
 
 if __name__ == "__main__":
-    mapmatch()
+    data = parse_csv("../tests/unit/test_data.csv")
+    print(data)
